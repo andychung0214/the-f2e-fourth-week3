56849f1d083a6e3f4804887d32afb6c922cbb6e3
Andy Chung <andy6756@gmail.com>
2022-11-21 17:12:58 +0800

    add guide component

---
+++ src/app/guide/guide.component.ts
@@ -0,0 +1,20 @@
+import { DialogService } from './../components/dialog/dialog.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guide',
+  templateUrl: './guide.component.html',
+  styleUrls: ['./guide.component.scss']
+})
+export class GuideComponent implements OnInit {
+  desc?:string;
+
+  constructor(private dialogservice: DialogService) { }
+
+  ngOnInit(): void {
+  }
+
+  openDialog(des: string){
+    this.dialogservice.openDialog(this.desc);
+  }
+}

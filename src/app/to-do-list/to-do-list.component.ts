25f543d504e329e99bbabeee3531012053e4b1f1
Andy Chung <andy6756@gmail.com>
2022-11-27 14:20:14 +0800

    update to do list page

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -1,4 +1,6 @@
+import { listPokerCard } from './../models/listPokerCard.model';
 import { Component, OnInit } from '@angular/core';
+import { Options } from 'sortablejs';
 
 @Component({
   selector: 'app-to-do-list',
@@ -6,6 +8,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./to-do-list.component.scss']
 })
 export class ToDoListComponent implements OnInit {
+  list1 = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+  ];
+
+  list2 = [
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+  ];
+
+  list: listPokerCard[] = [
+    {
+      pokerNum : 'A',
+      pokerText: '會員系統\n(登入、註冊、權限管理)',
+      children: []
+    },
+    {
+      pokerNum : 'A',
+      pokerText: '應徵者的線上履歷編輯器',
+      children: []
+    },
+    {
+      pokerNum : 'A',
+      pokerText: '前台職缺列表(職缺詳細內容、點選可發送應徵意願)',
+      children: []
+    },
+    {
+      pokerNum : 'A',
+      pokerText: '後台職缺管理功能(資訊上架、下架、顯示應徵者資料)',
+      children: []
+    }
+  ]
+
+  options: Options = {
+    group: {
+      name: "task",
+      put: true
+    },
+    revertOnSpill: true,
+    multiDrag: true
+  };
 
   constructor() { }
 

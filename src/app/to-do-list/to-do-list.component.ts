34ee0578ea42c628021ef6971a5e71a341b4990e
Andy Chung <andy6756@gmail.com>
2022-11-28 01:03:42 +0800

    update to do list component

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -8,21 +8,6 @@ import { Options } from 'sortablejs';
   styleUrls: ['./to-do-list.component.scss']
 })
 export class ToDoListComponent implements OnInit {
-  list1 = [
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-  ];
-
-  list2 = [
-    '6',
-    '7',
-    '8',
-    '9',
-    '10',
-  ];
 
   list: listPokerCard[] = [
     {

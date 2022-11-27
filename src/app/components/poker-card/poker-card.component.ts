540610cbdc29c34df261c00c384bddd95abc481d
Andy Chung <andy6756@gmail.com>
2022-11-27 14:19:42 +0800

    update poker card component

--- src/app/components/poker-card/poker-card.component.ts
+++ src/app/components/poker-card/poker-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {Options} from 'sortablejs';
+import { listPokerCard } from 'src/app/models/listPokerCard.model';
 
 @Component({
   selector: 'app-poker-card',
@@ -8,7 +10,16 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PokerCardComponent implements OnInit {
   @Input() pokerText?:string;
   @Input() pokerNum?:string;
+  @Input() listPokerCard?: listPokerCard;
 
+  options: Options = {
+    group: {
+      name: 'task',
+      put: true
+    },
+    revertOnSpill: true,
+    multiDrag: true
+  };
   constructor() { }
 
   ngOnInit(): void {
